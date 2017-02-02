98594213882f372b03ee8d2538012c0887fee4ad
powenchen <chen.pwen@gmail.com>
2017-02-01 18:56:49 -0800

    assets

---
+++ Lowpoly_Sports_Car_2_in_1/mover.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mover : MonoBehaviour
+{
+    public float forwardSpeed ;
+    public float backwardSpeed ;
+    public float rotateSpeed ;
+
+    // Use this for initialization 
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position -= Quaternion.Euler(0, -90, 0) *(transform.forward * forwardSpeed )* Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Quaternion.Euler(0, -90, 0) * transform.forward * backwardSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0.0f, -rotateSpeed * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0.0f, rotateSpeed * Time.deltaTime, 0.0f);
+        }
+       
+    }
+}
---
+++ goal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class goal : MonoBehaviour {
+    public Text goalTxt;
+	// Use this for initialization
+	void Start () {
+        goalTxt.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag("Player"))
+        {
+            goalTxt.text = "YOU WIN!!";
+
+        }
+    }
+}
