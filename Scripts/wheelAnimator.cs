da8ab2557fd78bdd8a96fd5b20b74a708d1da740
powenchen <chen.pwen@gmail.com>
2017-02-04 14:55:53 -0800

    scipts folder created
    scipts folder created

---
+++ Scripts/wheelAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wheelAnimator : MonoBehaviour {
+    private WheelCollider wheel;
+	// Use this for initialization
+	void Start () {
+        wheel = GetComponent<WheelCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward * Time.deltaTime * wheel.rpm / 60f * 360f);
+        Debug.Log(wheel.rpm / 60f * 360f * Time.deltaTime);
+        transform.localRotation = Quaternion.Euler(90,270, -wheel.steerAngle);
+    }
+}
