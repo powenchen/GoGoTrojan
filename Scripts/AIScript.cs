436c70e00e867455977995ee52314eecf1a0df26
powenchen <chen.pwen@gmail.com>
2017-02-06 08:15:56 -0800

    ai avoiding/ sensor
    sensor v1

--- Scripts/AIScript.cs
+++ Scripts/AIScript.cs
@@ -19,12 +19,20 @@ public class AIScript : MonoBehaviour {
     public float speedThreshold = 1;
     public int stepsBelowThreshold = 15, stepsAboveThreshold = 12;
 	public float  forwardStiffness = 5,sidewayStiffness = 10;
+    public int nextSteps = 3;
 
     public Vector3 com;
     public GameObject frontLeft, frontRight, rearLeft, rearRight;
     private Rigidbody rb;
     private WheelController flController, frController, rlController, rrController;
-    
+
+    public float sensorLength = 5;
+    public float sideSensorLength = 3;
+    public float frontSensorStartPoint = 3;
+    public float frontSensorMargin = 1;
+    public float frontSensorAngle = 15;
+    public float avoidSpeed = 10;
+    private int flag = 0;
 
     // Use this for initialization
     void Start () {
@@ -62,44 +70,103 @@ public class AIScript : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        if (pathPointIdx < pathPoints.Length)
-        {
-            Vector3 steerVector = transform.InverseTransformPoint(new Vector3(
-                                                        pathPoints[pathPointIdx].position.x,
-                                                        transform.position.y,
-                                                        pathPoints[pathPointIdx].position.z
-                                                        ));
-            float newSteer = maxSteer * (steerVector.x / steerVector.magnitude);
-
-            flController.ApplySteer(newSteer);
-            frController.ApplySteer(newSteer);
-            
-            float newMotorTorque = maxMotorTorque * (1 - Mathf.Abs(steerVector.x / steerVector.magnitude));
-            flController.ApplyThrottle(newMotorTorque);
-            frController.ApplyThrottle(newMotorTorque);
-            rlController.ApplyThrottle(newMotorTorque);
-            rrController.ApplyThrottle(newMotorTorque);
-            
-
-            Vector3 distToPoint = transform.position - pathPoints[pathPointIdx].position;
-            if (distToPoint.magnitude < distThreshold || steerVector.z < 0)
-            {
-               // Debug.Log("passed point" + pathPointIdx + "[" + pathPoints[pathPointIdx].position.x + "," + pathPoints[pathPointIdx].position.y + "," + pathPoints[pathPointIdx].position.z + "]");
-                ++pathPointIdx;
-            }
-        }
-        else
+        if (pathPointIdx >= pathPoints.Length)
         {
             flController.ApplyBrake(maxBrakeTorque);
             frController.ApplyBrake(maxBrakeTorque);
             rlController.ApplyBrake(maxBrakeTorque);
             rrController.ApplyBrake(maxBrakeTorque);
+            return;
         }
-        if (rb.velocity.magnitude > topSpeed)
+        if (flag == 0)
         {
-            float slowDownRatio = rb.velocity.magnitude / topSpeed;
-            rb.velocity /= slowDownRatio;
+            if (pathPointIdx < pathPoints.Length)
+            {
+                bool isReverse = false;
+                int bestIdx = pathPointIdx;
+
+                Vector3 bestPoint = transform.InverseTransformPoint(new Vector3(
+                                                            pathPoints[pathPointIdx].position.x,
+                                                            transform.position.y,
+                                                            pathPoints[pathPointIdx].position.z
+                                                            ));
+                for (int i = pathPointIdx + 1; i < pathPoints.Length && i < pathPointIdx + nextSteps; ++i)
+                {
+                    Vector3 point = transform.InverseTransformPoint(new Vector3(
+                                                            pathPoints[i].position.x,
+                                                            transform.position.y,
+                                                            pathPoints[i].position.z
+                                                            ));
+                    if (point.z > 0)
+                    {
+                        if ((point.x / point.z) < (bestPoint.x / bestPoint.z))
+                        {
+                            bestPoint = point;
+                            bestIdx = i;
+                        }
+                    }
+                }
+
+                if (bestPoint.z < -distThreshold)
+                {
+                    isReverse = true;
+                }
+                else
+                {
+                    pathPointIdx = bestIdx;
+                    Debug.Log("go to point" + bestIdx);
+                }
+
+
+
+                Vector3 steerVector = transform.InverseTransformPoint(new Vector3(
+                                                            pathPoints[pathPointIdx].position.x,
+                                                            transform.position.y,
+                                                            pathPoints[pathPointIdx].position.z
+                                                            ));
+
+                //Debug.Log(steerVector.ToString());
+                float newSteer = maxSteer * (steerVector.x / steerVector.magnitude);
+
+                float newMotorTorque = maxMotorTorque * (1 - Mathf.Abs(steerVector.x / steerVector.magnitude));
+
+                if (isReverse)
+                {
+                    Debug.Log("isReverse; go to point" + bestPoint.x + "," + bestPoint.z);
+                    flController.ApplySteer(-maxSteer * Mathf.Sign(newSteer));
+                    frController.ApplySteer(-maxSteer * Mathf.Sign(newSteer));
+
+                    flController.ApplyThrottle(-maxMotorTorque * 0.5f);
+                    frController.ApplyThrottle(-maxMotorTorque * 0.5f);
+                    rlController.ApplyThrottle(-maxMotorTorque * 0.5f);
+                    rrController.ApplyThrottle(-maxMotorTorque * 0.5f);
+                }
+                else if (bestPoint.magnitude < distThreshold)
+                {
+                    Debug.Log("passed point" + pathPointIdx + "[" + pathPoints[pathPointIdx].position.x + "," + pathPoints[pathPointIdx].position.y + "," + pathPoints[pathPointIdx].position.z + "]");
+
+                    ++pathPointIdx;
+                }
+
+                else
+                {
+                    flController.ApplySteer(newSteer);
+                    frController.ApplySteer(newSteer);
+
+                    flController.ApplyThrottle(newMotorTorque);
+                    frController.ApplyThrottle(newMotorTorque);
+                    rlController.ApplyThrottle(newMotorTorque);
+                    rrController.ApplyThrottle(newMotorTorque);
+                }
+            }
+            if (rb.velocity.magnitude > topSpeed)
+            {
+                float slowDownRatio = rb.velocity.magnitude / topSpeed;
+                rb.velocity /= slowDownRatio;
+            }
         }
+
+        Sensor();
     }
 
 
@@ -111,6 +178,120 @@ public class AIScript : MonoBehaviour {
     public void removeDebuff(float debuffRatio)
     {
         topSpeed *= debuffRatio;
+    }
+
+    public void Sensor()
+    {
+        flag = 0;
+        float avoidSensitivity = 0;
+        Vector3 pos;
+        pos = transform.position;
+        pos += transform.forward * frontSensorStartPoint;
+        RaycastHit hit;
+        
+        //braking sensor
+        if (Physics.Raycast(pos, transform.forward, out hit, sensorLength) && !hit.transform.CompareTag("Terrain"))
+        {
+            ++flag;
+            flController.ApplyBrake(maxBrakeTorque * 0.2f);
+            frController.ApplyBrake(maxBrakeTorque * 0.2f);
+            rlController.ApplyBrake(maxBrakeTorque * 0.2f);
+            rrController.ApplyBrake(maxBrakeTorque * 0.2f);
+            Debug.DrawLine(pos, hit.point, Color.red);
+        }
+        else
+        {
+            flController.ApplyBrake(0);
+            frController.ApplyBrake(0);
+            rlController.ApplyBrake(0);
+            rrController.ApplyBrake(0);
+        }
+        
+
+        //front right sensor and right angled sensor
+        pos = transform.position;
+        pos += transform.forward * frontSensorStartPoint + transform.right*frontSensorMargin;
+        Vector3 sensorAngle = Quaternion.AngleAxis(frontSensorAngle, transform.up) * transform.forward;
+        if (Physics.Raycast(pos, transform.forward, out hit, sensorLength) && !hit.transform.CompareTag("Terrain"))
+        {
+            ++flag;
+            avoidSensitivity -= 1;
+            Debug.DrawLine(pos, hit.point, Color.white);
+        }
+        else if (Physics.Raycast(pos, sensorAngle, out hit, sensorLength) && !hit.transform.CompareTag("Terrain"))
+        {
+            ++flag;
+            avoidSensitivity -= 0.5f;
+            Debug.DrawLine(pos, hit.point, Color.white);
+        }
+
+
+        //front left sensor
+        pos = transform.position;
+        pos += transform.forward * frontSensorStartPoint - transform.right * frontSensorMargin;
+
+        sensorAngle = Quaternion.AngleAxis(-frontSensorAngle, transform.up) * transform.forward;
+        if (Physics.Raycast(pos, transform.forward, out hit, sensorLength) && !hit.transform.CompareTag("Terrain"))
+        {
+            ++flag;
+            avoidSensitivity += 1;
+            Debug.DrawLine(pos, hit.point, Color.white);
+        }
+        else if (Physics.Raycast(pos, sensorAngle, out hit, sensorLength) && !hit.transform.CompareTag("Terrain"))
+        {
+            ++flag;
+            avoidSensitivity += 0.5f;
+            Debug.DrawLine(pos, hit.point, Color.white);
+        }
+
+        //right side sensor
+        if (Physics.Raycast(transform.position, transform.right, out hit, sideSensorLength) && !hit.transform.CompareTag("Terrain"))
+        {
+            ++flag;
+            avoidSensitivity -= 0.5f;
+            Debug.DrawLine(transform.position, hit.point, Color.white);
+        }
+
+        //left side sensor
+        if (Physics.Raycast(transform.position, -transform.right, out hit, sideSensorLength) && !hit.transform.CompareTag("Terrain"))
+        {
+            ++flag;
+            avoidSensitivity += 0.5f;
+            Debug.DrawLine(transform.position, hit.point, Color.white);
+        }
+
+        pos = transform.position;
+        pos += transform.forward * frontSensorStartPoint;
+        //front mid sensor
+        if (avoidSensitivity == 0)
+        {
+            if (Physics.Raycast(pos, transform.forward, out hit, sensorLength))
+            {
+                if (hit.normal.x < 0)
+                {
+                    avoidSensitivity = 1;
+                }
+                else
+                {
+                    avoidSensitivity = -1;
+                }
+                Debug.DrawLine(pos, hit.point, Color.white);
+            }
+        }
+
+        if (flag != 0)
+        {
+            AvoidSteer(avoidSensitivity);
+        }
+
+    }
+
+    void AvoidSteer(float sensitivity)
+    {
+        float newSteer = avoidSpeed * sensitivity;
+        Debug.Log("avoid steer " + newSteer);
+        flController.ApplySteer(newSteer);
+        frController.ApplySteer(newSteer);
     }
 
 
